4c52a3640766af5777fb11fd488d589eb4549767
user-q1OaB78w <143874187+user-q1OaB78w@users.noreply.github.com>
2024-02-17 16:11:01 +0800

    update

--- swiper.js
+++ swiper.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper('.swiper',
+const mobileSwiper = new Swiper('.mobile-swiper',
 {
     direction: 'vertical',
     loop: false,
@@ -6,7 +6,45 @@ const swiper = new Swiper('.swiper',
     spaceBetween: 250,
     pagination:
     {
-      el: '.swiper-pagination',
+      el: '.mobile-swiper-pagination',
       clickable: true,
+    },
+    navigation:
+    {
+      nextEl: ".swiper-button-next",
+    },
+});
+
+const desktopSwiper = new Swiper('.desktop-swiper',
+{
+    direction: 'vertical',
+    loop: false,
+    mousewheel: true,
+    spaceBetween: 250,
+    pagination:
+    {
+      el: '.desktop-swiper-pagination',
+      clickable: true,
+    },
+});
+
+const reviewSwiper = new Swiper('.review-swiper',
+{
+    direction: 'horizontal',
+    loop: false,
+    spaceBetween: 50,
+    slidesPerView: 'auto',
+    autoplay:
+    {
+      delay: 3000,
+      disableOnInteraction: false,
+      reverseDirection: false,
+    },
+    speed: 2000,
+    pagination:
+    {
+      el: '.review-swiper-pagination',
+      clickable: true,
+      dynamicBullets: true,
     },
 });
