6ed420e65e56c311c27ad77d60988844e4876059
user-q1OaB78w <143874187+user-q1OaB78w@users.noreply.github.com>
2024-02-21 21:03:02 +0800

    update

--- header.js
+++ header.js
@@ -20,3 +20,26 @@ function updateSwiperHeight()
         slide.style.paddingTop = `${headerHeight}px`;
     });
 }
+
+document.addEventListener('DOMContentLoaded', function ()
+{
+    updateMobileSwiperHeight();
+
+    window.addEventListener('resize', function ()
+    {
+        updateMobileSwiperHeight();
+    });
+});
+
+function updateMobileSwiperHeight()
+{
+    const header = document.querySelector('mobile-header');
+    const headerHeight = header.clientHeight;
+
+    const swiperSlides = document.querySelectorAll('.mobile-header-padding');
+
+    swiperSlides.forEach(function(slide)
+    {
+        slide.style.paddingTop = `${headerHeight}px`;
+    });
+}
--- multicolumn.js
+++ multicolumn.js
@@ -15,3 +15,21 @@ document.getElementById('column-3').addEventListener('mouseenter', function()
     const videoPlayer = document.querySelector('.video-3');
     videoPlayer.play();
 });
+
+document.getElementById('mobile-column-1').addEventListener('mouseenter', function()
+{
+    const videoPlayer = document.querySelector('.mobile-video-1');
+    videoPlayer.play();
+});
+
+document.getElementById('mobile-column-2').addEventListener('mouseenter', function()
+{
+    const videoPlayer = document.querySelector('.mobile-video-2');
+    videoPlayer.play();
+});
+
+document.getElementById('mobile-column-3').addEventListener('mouseenter', function()
+{
+    const videoPlayer = document.querySelector('.mobile-video-3');
+    videoPlayer.play();
+});
